1f6cf81059bfe035dd0726e99492d1627076b0e5
Alex <sascha.panin2011@yandex.ru>
2020-05-06 15:57:06 +0500

    Scrollbar fixed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-app.use(serveStatic(__dirname + "/build"))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+let express = require('express');
+let path = require('path');
+let serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/build"));
+let port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started ' + port);
